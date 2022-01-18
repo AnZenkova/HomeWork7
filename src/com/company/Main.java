c88216801dc40f16dab771b2e80d7c8c2103b2e4
AnZenkova <nastyonka16@mail.ru>
2022-01-18 20:18:16 +1000

    Первое задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstName = "Ivan";
+        firstName = firstName + ' ';
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        lastName = lastName + ' ';
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
